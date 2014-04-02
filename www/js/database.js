e6ee4da41610bfcc17ceba8629a40f0e3b13a26c
thangpqfuhl <thangpq@outlook.com.vn>
2014-04-02 16:02:23 +0700

    Change "finished" input to combo box, add "finished" in db, save to db ok

--- www/js/database.js
+++ www/js/database.js
@@ -2,21 +2,14 @@
 
 // Wait for PhoneGap to load
 var db = null;
-var dbCreated = false;
 document.addEventListener("deviceready", onDeviceReady, false);
-var category = "";
 // PhoneGap is ready
 //
 function onDeviceReady() {
-	db = window.openDatabase("HomeworkTracker2", "2.0", "HomeworkTracker2", 2000);
+	db = window.openDatabase("HomeworkTracker3", "2.0", "HomeworkTracker3", 2000);
 	db.transaction(populateDB, errorCB, successCB);
 }
 
-
-function getDeadlines(tx){
-	var sql = "select distinct cName from COCKTAIL where cCategory = '"+category+"'";
-	tx.executeSql(sql, [] , getAlcohol_success);
-}
 
 function randomString(L){
     var s= '';
@@ -45,26 +38,18 @@ function saveDeadlineToDB(){
 	alert(dbType);
 	var dbAdditionalInfo = document.getElementById("additionalInfo").value;
 	alert(dbAdditionalInfo);
-	insertDeadlineToDB(dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbType, dbAdditionalInfo);
+	var dbFinished = document.getElementById("finished").value;
+	alert(dbFinished);
+	insertDeadlineToDB(dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbType, dbAdditionalInfo, dbFinished);
 
 }
 
-function getAlcohol_success(tx, results){
-	//alert('get alcohol success');
-	var len = results.rows.length;
-	//alert(len);
-	for (var i=0; i<len; i++){
-		var alcohol = results.rows.item(i);
-		//alert('before append');
-		$('#itemList').append('<li><a href="location.html?name=' + alcohol.cName + '&category='+category+'"><p>' + alcohol.cName + '</p></li>');
-	}
-
-	alert('before append');
-}
 // Populate the database
-//
+
 function populateDB(tx) {
-	 tx.executeSql('CREATE TABLE IF NOT EXISTS deadlines (id varchar(10) primary key, description varchar(500), class varchar(50), duedate date, duetime time, type varchar(50), additionalInfo varchar(200))');
+	alert('starting populate');
+	 tx.executeSql('CREATE TABLE IF NOT EXISTS deadlines (id varchar(10) primary key, description varchar(500), class varchar(50), duedate date, duetime time, type varchar(50), additionalInfo varchar(200), finished varchar(10))');
+	 alert(tx);
 }
 
 // Transaction error callback
@@ -77,16 +62,15 @@ function successCB() {
 	alert('success');
 }
 
-function insertDeadlineToDB(dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbType, dbAdditionalInfo) {
+function insertDeadlineToDB(dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbType, dbAdditionalInfo, dbFinished) {
 	//alert('insert called');
 	alert('before populate');
 	db.transaction(populateDB, errorCB, successCB);
 	alert('before insert');
 	db.transaction(function(tx){
-		tx.executeSql('INSERT INTO deadlines (id, description, class, dueDate, dueTime, type, additionalInfo) VALUES (?,?,?,?,?,?,?)',[dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbType, dbAdditionalInfo],successCB, errorCB);
+		tx.executeSql('INSERT INTO deadlines (id, description, class, dueDate, dueTime, type, additionalInfo, finished) VALUES (?,?,?,?,?,?,?,?)',[dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbType, dbAdditionalInfo, dbFinished],successCB, errorCB);
 		alert(tx);
    });
-   window.location.replace("deadlines.html");
 }
 
 
