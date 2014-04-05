735a9c16d7d332c52fc02e8e0424e72b07696314
thangpqfuhl <thangpq@outlook.com.vn>
2014-04-05 18:10:42 +0700

    Filter list for "homework"
    Only display records that has deadline time > current time

--- www/js/deadlinesDB.js
+++ www/js/deadlinesDB.js
@@ -13,11 +13,13 @@ function populateDB(tx) {
 }
 
 function getDeadlines(tx){
+	alert('get deadline');
 	var sql = "select * from deadlines where finished = 'no'";
 	tx.executeSql(sql, [] , getAllDeadlines_success);
-	var sql2 = "select * from deadlines where type = 'Homework'";
+	var sql2 = "select * from deadlines where finished = 'no' and type = 'Homework'";
 	tx.executeSql(sql2, [] , getHomeworkDeadlines_success);
-	var sql3 = "select * from deadlines where type = 'Test'";
+	alert('get test deadline');
+	var sql3 = "select * from deadlines where finished = 'no' and type = 'Test'";
 	tx.executeSql(sql3, [] , getTestDeadlines_success);
 }
 
@@ -34,12 +36,44 @@ function getAllDeadlines_success(tx, results){
 		//alert('before append');
 }
 function getHomeworkDeadlines_success(tx, results){
-
+	
+	alert('get homework deadlines');
 	var len = results.rows.length;
 	//var s = "";
+	var now = new Date();
+	var year = now.getFullYear();
+	var month = now.getMonth() + 1;
+	var date = now.getDate();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	alert ('Today: ' + year + ' ' + month + ' ' + date );
+	
 	for (var i=0; i<len; i++){
+		
 		var homeworkDeadline = results.rows.item(i);
-			
+		var parts = homeworkDeadline.duedate.split('-');
+		var time = homeworkDeadline.duetime.split(':');
+		
+		alert('deadline year: '+ parts[0]);
+		alert('deadline month: ' + parts[1]);
+		alert('deadline date: ' + parts[2]);
+		alert('deadline hour: ' + time[0]);
+		alert('deadline minute: ' + time[1]);
+		
+		if ( parts[0] < year ){// previous year
+			continue;
+		} else if ( ( parts[0] == year ) && ( parts[1] < month)){ // previous month
+			continue;
+		} else if (( parts[0] == year ) && ( parts[1] == month) && (parts[2] < date)){// previous date
+			continue;
+		} else if (( parts[0] == year ) && ( parts[1] == month) && (parts[2] == date) && (time[0] < hour)){ // previous hour
+			continue;
+		} else if (( parts[0] == year ) && ( parts[1] == month) && (parts[2] == date) && (time[0] == hour) && (time[1] < minute)) { // previous minute
+			continue;
+		}
+				
+		alert('prepend');		
+		
 		$('#homeworkList').prepend('<li><a href="deadlineDetail.html?id='+ homeworkDeadline.id+'">'+ homeworkDeadline.class + '<br>' + homeworkDeadline.duedate+'    '+ homeworkDeadline.duetime+'<br>'+ homeworkDeadline.description +'</a></li>');
 		
 	}
