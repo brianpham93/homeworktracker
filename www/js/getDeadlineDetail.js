4907902857fc8f38492bece994f8933619ac776a
thangpqfuhl <thangpq@outlook.com.vn>
2014-04-06 16:10:05 +0700

    add values to fields in deadlineDetail.html
    Problem: Cannot select the select box

---
+++ www/js/getDeadlineDetail.js
@@ -0,0 +1,114 @@
+var db = null;
+document.addEventListener("deviceready", onDeviceReady, false);
+// PhoneGap is ready
+//
+function onDeviceReady() {
+	db = window.openDatabase("HomeworkTracker3", "2.0", "HomeworkTracker3", 2000);
+	db.transaction(populateDB, errorCB, successCB);
+	alert('populate db done');
+	db.transaction(populateClassDB, errorCB, successCB);
+	alert('populate class db done');
+	db.transaction(getClasses, errorCB);
+	db.transaction(getDeadlineDetail, errorCB);
+	
+}
+
+function populateDB(tx) {
+	 tx.executeSql('CREATE TABLE IF NOT EXISTS deadlines (id varchar(10) primary key, description varchar(500), class varchar(50), duedate date, duetime time, type varchar(50), additionalInfo varchar(200), finished varchar(10))');
+}
+
+function populateClassDB(tx) {
+	//alert('starting populate');
+	 tx.executeSql('CREATE TABLE IF NOT EXISTS classes (id varchar(10) primary key, name varchar(50), location varchar(50), classdate varchar(50), classtime time, teacher varchar(50), email varchar(200), phone varchar(10))');
+	 alert('populate done');
+	 //alert(tx);
+}
+
+function getClasses(tx){
+	alert('classes');
+	var sql = "select * from classes";
+	tx.executeSql(sql, [] , getClasses_success);
+	
+}
+
+function getClasses_success(tx, results){
+
+	var len = results.rows.length;
+	alert('len: ' + len);
+	//var s = "";
+	for (var i=0; i<len; i++){
+		var classDB = results.rows.item(i);
+		$('#class').append('<option value="'+ classDB.name + '">'+ classDB.name +'</option>');
+	}
+		//alert('before append');
+}
+
+function getDeadlineDetail(tx){
+	alert('get deadline detail');
+	var id = GET.id;
+	alert(id);
+	var sql = "select * from deadlines where id = '" + id +"'";
+	tx.executeSql(sql, [] , getDeadlineDetail_success);
+}
+
+function getDeadlineDetail_success(tx, results){
+
+	var len = results.rows.length;
+	//var s = "";
+	for (var i=0; i<len; i++){
+		var deadline = results.rows.item(i);
+		var description = deadline.description;
+		var classDeadline = deadline.class;
+		var duedate = deadline.duedate;
+		var duetime = deadline.duetime;
+		var type = deadline.type;
+		var additionalInfo = deadline.additionalInfo;
+		var finished = deadline.finished;
+		
+		document.getElementById("shortDescription").value = description;
+		document.getElementById("dueDate").value = duedate;
+		document.getElementById("dueTime").value = duetime;
+		document.getElementById("additionalInfo").value = additionalInfo;
+		$("#class").val(classDeadline);
+		//document.getElementById("class").selected = classDeadline;
+		document.getElementById("type").selected = type;
+		document.getElementById("finished").selected = finished;
+	
+	}
+		//alert('before append');
+}
+
+GET = (function () {
+    var get = {
+        push:function (key,value){
+            var cur = this[key];
+            if (cur.isArray){
+                this[key].push(value);
+            }else {
+                this[key] = [];
+                this[key].push(cur);
+                this[key].push(value);
+            }
+        }
+    },
+    search = document.location.search,
+    decode = function (s,boo) {
+        var a = decodeURIComponent(s.split("+").join(" "));
+        return boo? a.replace(/\s+/g,''):a;
+    };
+    search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function (a,b,c) {
+        if (get[decode(b,true)]){
+            get.push(decode(b,true),decode(c));
+        }else {
+            get[decode(b,true)] = decode(c);
+        }
+    });
+    return get;
+})();
+
+function errorCB(tx, err) {
+	alert("Error processing SQL: "+err);
+}
+
+function successCB(tx){
+}
