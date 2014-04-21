4b716901c932e78a9d0a19861f2cd328e24e6b24
thangpqfuhl <thangpq@outlook.com.vn>
2014-04-21 11:38:33 +0700

    Upgrade phonegap platform

--- platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphoneos/CordovaLib.build/Objects-normal/armv7s/CDVConfigParser.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/thangpq/homeworktracker/platforms/ios/CordovaLib/Classes/CDVConfigParser.m \
-  /Users/thangpq/homeworktracker/platforms/ios/CordovaLib/Classes/CDVConfigParser.h
